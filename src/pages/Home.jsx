53a97f67c0d16a919dc8f2ff339233874c0ee4c1
Raiyan109 <kraiyan109@gmail.com>
2023-10-28 12:40:12 +0600

    layout minus

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <Navbar />
+            kjkj
         </div>
     );
 };
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,18 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-import Layout from "../pages/Layout";
 import SignUp from "../pages/SignUp";
 import ForgotPassword from "../pages/ForgotPassword";
 
 const routes = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout>
-            <Home />
-            <Login />
-        </Layout>
-    },
     {
         path: '/',
         element: <Home />
